4206910a4f99764ecbaef87f16171384fc0d4c8f
shravan <bshravankumar0@gmail.com>
2024-03-23 20:43:47 +0530

    adding menu options in the application

--- EmailSender/src/main/java/com/techplement/EmailSender/App.java
+++ EmailSender/src/main/java/com/techplement/EmailSender/App.java
@@ -2,15 +2,17 @@ package com.techplement.EmailSender;
 
 import java.io.IOException;
 
+import com.techplement.controller.HomeController;
+
 public class App {
 
 	public static void main(String[] args) throws IOException {
 
-		String email = "dsuchithra670@gmail.com";
-		String subject = "Testing Email Sender";
-		String message = "Dear Candidate,\n\nThis is a test email...!";
-
-		EmailSender emailSender = new EmailSender();
-		emailSender.sendMessage(email, subject, message);
+		/*
+		 * String email = "dsuchithra670@gmail.com"; String subject =
+		 * "Testing Email Sender"; String message =
+		 * "Dear Candidate,\n\nThis is a test email...!";
+		 */
+		HomeController.startApp();
 	}
 }
---
+++ EmailSender/src/main/java/com/techplement/controller/HomeController.java
@@ -0,0 +1,53 @@
+package com.techplement.controller;
+
+import java.util.Scanner;
+import com.techplement.EmailSender.EmailSender;
+import com.techplement.utils.Utility;
+
+public class HomeController {
+
+	public static void startApp() {
+		EmailSender emailSender = new EmailSender();
+
+		Scanner scanner = new Scanner(System.in);
+		Utility.printWelcome();
+		Utility.printMenu();
+		String options = "-1";
+		while (!options.equals("2")) {
+			options = scanner.next();
+			try {
+				switch (options) {
+				case "1":
+					System.out.print("Enter recipient Mail : ");
+					String recipientMail = scanner.next();
+					scanner.nextLine(); // Consume newline
+					System.out.print("Enter Subject of the Mail : ");
+					String subject = scanner.nextLine(); // Capture entire line for subject
+					System.out.println("Enter the Message (type 'END' on a new line to finish): ");
+					StringBuilder messageBuilder = new StringBuilder();
+					String line;
+					while (!(line = scanner.nextLine()).equalsIgnoreCase("END")) {
+						messageBuilder.append(line).append("\n");
+					}
+					String message = messageBuilder.toString().trim(); // Remove trailing newline
+					emailSender.sendMessage(recipientMail, subject, message);
+					Utility.printMenu();
+					break;
+
+				case "2":
+					Utility.printQuitMsg();
+					break;
+
+				default:
+					System.err.println("Invalid option. Please try again.");
+					System.out.println("***************************");
+					Utility.printMenu();
+				}
+			} catch (Exception e) {
+				System.err.println("Invalid error occurred..!. Please try again.");
+				Utility.printMenu();
+			}
+		}
+		scanner.close();
+	}
+}
